d8759529b2cb3813611d8cb81e66b03534b721b3
iTalent <italent@iTalents-MacBook-Pro.local>
2026-03-16 16:44:28 +0530

    Added permalink

--- widgets/Hello_msft/dist/script.js
+++ widgets/Hello_msft/dist/script.js
@@ -16,7 +16,7 @@ initWidget();
     const sdk = new window.WidgetServiceSDK();
 
     const result = await sdk.connectors.execute({
-      permalink: "users-location",
+      permalink: "get-users",
       method: "GET"
     });
 
