50c666ffaf9800bbd21c1a28af39489836d816a7
iTalent <italent@iTalents-MacBook-Pro.local>
2026-03-16 16:40:47 +0530

    Added script

--- widgets/Hello_msft/dist/script.js
+++ widgets/Hello_msft/dist/script.js
@@ -1,7 +1,7 @@
 function initWidget() {
   console.log("Hello widget loaded");
 
-  const container = document.querySelector(".widget-container");
+  const container = document.querySelector(".widget-hello");
 
   if (container) {
     const msg = document.createElement("p");
